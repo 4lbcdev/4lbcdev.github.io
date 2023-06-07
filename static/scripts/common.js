166ef11e199a5822a312c9de1e593f2c334c0f01
leroysb <leroybuliro@gmail.com>
2023-06-07 22:08:42 +0300

    index page ux

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -12,4 +12,30 @@ $(document).ready(function() {
       $img.attr('src', './static/images/menu_open.png');
     }
   });
+  
+  // Header scroll event handler
+  $(window).scroll(function() {
+    var scrollPosition = $(window).scrollTop();
+    var $header = $('header');
+    var $menuLinks = $('.menu a');
+
+    // Check the scroll position
+    if (scrollPosition >= 100) {
+      // Change the header background color to white
+      $header.css('background-color', 'white');
+      // Add the .header_border class to the header
+      $header.addClass('header_border');
+      // Change the color of menu links to black
+      $menuLinks.css('color', '#606060');
+
+    } else {
+      // Reset the header background color to transparent
+      $header.css('background-color', 'transparent');
+      // Remove the .header_border class from the header
+      $header.removeClass('header_border');
+      // Change the color of menu links to white
+      $menuLinks.css('color', 'white');
+    }
+  });
+
 });
